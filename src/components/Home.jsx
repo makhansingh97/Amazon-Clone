20c5b00db57ac447d8b5277e5c210ea25c42c5f6
mskevat97 <msboss6r1ff1n@gmail.com>
2021-04-01 00:44:17 +0530

    safe point

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Home.css"
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/Feb/EN/1500x600_Hero-Tall_JN._CB655777066_.jpg"
+          alt="homepage"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
