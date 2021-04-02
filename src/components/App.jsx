e6f0d85e1c48414451fab0bc4f659c98fa55e9fb
mskevat97 <msboss6r1ff1n@gmail.com>
2021-04-02 16:33:52 +0530

    react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import "../styles/App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I'm checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
