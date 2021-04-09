6fbc5df7079170d0ef3e83bdc0ce0f4dab295f24
mskevat97 <msboss6r1ff1n@gmail.com>
2021-04-09 18:28:21 +0530

    Checkout page - ad, subtotal component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,18 @@ import "../styles/App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>I'm checkout</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
